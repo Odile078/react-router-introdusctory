7a4c28d14b144c3c3736402718e46c42663194a8
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-19 16:05:57 +0200

    feat: add Link for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
