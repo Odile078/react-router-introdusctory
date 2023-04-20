ce8747765394026772799ec912d3e92bd5f1fa48
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-20 16:00:06 +0200

    feat: add about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,33 @@
+import aboutImg from "../assets/images/aboutImg.png";
 const About = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">About page</h1>
+    <div className="pb-10 space-y-10">
+      <img
+        className="object-cover object-center w-full max-h-96"
+        src={aboutImg}
+      />
+      <div className="max-w-5xl mx-6 space-y-6 font-bold">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p className="text-sm text-gray-900">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p className="text-sm text-gray-800">
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </div>
+      <div className="p-6 mx-6 space-y-6 bg-orange-200">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Your destination is waiting. Your van is ready.
+        </h1>
+        <button className="px-5 py-2 text-base text-white bg-gray-900 rounded-md ">
+          Explore our vans
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,14 @@ import Logo from "../assets/images/logo-small.svg";
 const Navbar = () => {
   return (
     <div className="flex justify-between gap-4 px-4 py-9 bg-orange-50">
-      <img
-        src={Logo}
-        className="object-contain object-center w-32 sm:w-44"
-        alt="Logo"
-      />
+      <Link to="/" className="block">
+        <img
+          src={Logo}
+          className="object-contain object-center w-32 sm:w-44"
+          alt="Logo"
+        />
+      </Link>
+
       <div className="flex gap-6">
         {[
           { name: "About", url: "/about" },
