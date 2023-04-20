93b22e79fbaf5428e6bbe2d3a2cfd840bfcb7b66
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-20 14:58:46 +0200

    feat: add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
-
+import Logo from "../assets/images/logo-small.svg";
 const Navbar = () => {
   return (
-    <div className="flex">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <div className="flex justify-between gap-4 px-4 py-9 bg-orange-50">
+      <img
+        src={Logo}
+        className="object-contain object-center w-32  sm:w-44"
+        alt="Logo"
+      />
+      <div cl>
+        {[
+          { name: "Home", url: "/" },
+          { name: "About", url: "/about" },
+        ].map((link, index) => (
+          <Link
+            key={index}
+            to={link.url}
+            className="text-xl text-slate-600 hover:underline hover:text-slate-800"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
