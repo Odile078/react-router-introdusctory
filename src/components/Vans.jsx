2f739176a856a26b455e523c2606034b6b12253e
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-24 17:42:46 +0200

    feat: add van filters

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,7 +1,23 @@
 const Vans = () => {
+  const filters = ["simple", "luxury", "rugged"];
   return (
     <div className="flex-1 p-4 my-6 space-y-10 text-4xl font-bold ">
-      <h1 className="text-3xl font-bold">Explore our van options</h1>
+      <div className="space-y-6">
+        <h1 className="text-3xl font-bold">Explore our van options</h1>
+        <div className="flex flex-wrap justify-between gap-4 md:items-center">
+          <div className="flex flex-wrap gap-4 ">
+            {filters.map((filter, index) => (
+              <button
+                key={index}
+                className="px-5 py-1 text-sm bg-orange-100 rounded"
+              >
+                {filter}
+              </button>
+            ))}
+          </div>
+          <button className="text-sm underline">Clear filters</button>
+        </div>
+      </div>
     </div>
   );
 };
