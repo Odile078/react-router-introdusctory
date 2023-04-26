c8472e6ff6209fb30ca4ce89838ead1494a0a9a8
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-04-26 15:53:08 +0200

    feat: use relative paths in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,14 @@ function App() {
       <Routes>
         <Route element={<BlankLayout />}>
           <Route index path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetails />} />
-          <Route element={<HostLayout />}>
-            <Route index path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/vans" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetails />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="vans" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
