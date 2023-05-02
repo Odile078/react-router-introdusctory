8e2f61a06d498590279b5845dadcedc5ea008c5a
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-05-02 17:54:59 +0200

    feat: update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div className="m-auto ">
-      <h1 className="text-4xl font-bold text-center">Page not found</h1>
+      <h1 className="text-3xl font-bold text-center">
+        Sorry the Page you were looking for was not found.
+      </h1>
+      <p className="mt-2 text-center text-gray-600">
+        return <Link to="/">Home</Link>
+      </p>
     </div>
   );
 }
