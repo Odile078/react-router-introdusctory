3581c010681a7cbdf176f7d7360c82bdda6573ff
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-05-02 17:49:30 +0200

    feat: add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import HostVanDetails from "./pages/host/vans/HostVanDetails";
 import HostVanInfo from "./pages/host/vans/HostVanInfo";
 import HostVanPrice from "./pages/host/vans/HostVanPrice";
 import HostVanPhoto from "./pages/host/vans/HostVanPhoto";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
             </Route>
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="m-auto ">
+      <h1 className="text-4xl font-bold text-center">Page not found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
